4b0b7c14fe5ddba342b719039777b6e7c7bd9d2b
Dennis Heimbigner <dmh@ucar.edu>
2017-11-16 19:00:43 -0700

    missing file

---
+++ cf.cmake
@@ -0,0 +1,25 @@
+# Visual Studio
+VS=1
+
+CFG="Release"
+
+FLAGS="$FLAGS -DCMAKE_INSTALL_PREFIX=`pwd`/ignore"
+
+rm -fr build
+mkdir build
+cd build
+
+if test "x$VS" != x ; then
+# Visual Studio
+CFG="Release"
+cmake $FLAGS ..
+cmake --build . --config ${CFG}
+cmake --build . --config ${CFG} --target RUN_TESTS
+else
+# GCC
+G="-GUnix Makefiles"
+cmake "${G}" $FLAGS ..
+#make all
+#make test
+fi
+exit
